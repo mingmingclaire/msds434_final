bf6e519e4a17fac648647564d8e55ed5a47a66ba
mingmingclaire <tjgumingming@berkeley.edu>
2023-08-27 15:49:25 -0700

    code update

--- main.py
+++ main.py
@@ -1,134 +1,80 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #from flask import jsonify
 import google.auth
+import pandas as pd
 from google.cloud import bigquery
+import sys 
 
 app = Flask(__name__)
 
 credentials, project_id = google.auth.default(
     scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
-project_id = 'week7-395422'
+project_id = 'finalproject-396805'
 bqclient = bigquery.Client(credentials= credentials,project=project_id)
 
-# @app.route('/get_data')
-# def form():
-#     return render_template('input.html')
+@app.route('/get_data')
+def input():
+    return render_template('input.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 
+    if request.method == 'POST':
+        data = request.form
+        user_review = str(data['review_text'])
 
+        prediction_input = [{
+            "review_text": user_review
+        }]
 
+        df = pd.DataFrame(
+            prediction_input,
+            columns=[
+                "review_text"
+            ]
+        )
+    
+    job_config = bigquery.LoadJobConfig(
+        schema = [
+            bigquery.SchemaField('review_text', bigquery.enums.SqlTypeNames.STRING)
+        ],
+        write_disposition = 'WRITE_TRUNCATE',
+    )
+    
+    ingest = bqclient.load_table_from_dataframe(
+        df, 'finalproject-396805.imdb_movie_reviews.movie_review_input', job_config=job_config
+    )
 
-    #get predicted cases
+    ingest.result()
+
     query_string= """
     SELECT
-    DATE(forecast_timestamp) as PredDate, round(m.forecast_value) as Cases
+        predicted_review_label
     FROM
-    ML.FORECAST(MODEL Covid19NYT.train_model,
-                STRUCT(30 AS horizon, 0.8 AS confidence_level)) m
-    where m.forecast_timestamp = (select max(forecast_timestamp)
-    FROM
-    ML.FORECAST(MODEL Covid19NYT.train_model,
-                STRUCT(30 AS horizon, 0.8 AS confidence_level))
-    )
+        ML.PREDICT(MODEL `finalproject-396805.imdb_movie_reviews.train_model1`,
+            ( 
+                SELECT 
+                    ML.NGRAMS(bt.words_array, [1,2]) as ngrams
+                FROM  
+                    (SELECT 
+                        REGEXP_EXTRACT_ALL(LOWER(r.review_text ), '[a-z]+') as words_array
+                    FROM (SELECT string_field_0 as review_text 
+                          FROM `finalproject-396805.imdb_movie_reviews.movie_review_new_dp`
+                          WHERE string_field_0 != 'review_text') r
+                    ) bt
+            )
+        ) 
     """
     query_job = bqclient.query(query_string)
     results = query_job.result()
-    for row in results:
-        CasesPredictionDate = row.PredDate
-        PredCases = row.Cases
-
-        
-    # #get predicted deaths
-    # query_string= """
-    # SELECT
-    # DATE(forecast_timestamp) as PredDate, round(m.forecast_value) as Deaths
-    # FROM
-    # ML.FORECAST(MODEL Covid19NYT.deaths_arima_model,
-    #             STRUCT(30 AS horizon, 0.8 AS confidence_level)) m
-    # where m.forecast_timestamp = (select max(forecast_timestamp)
-    # FROM
-    # ML.FORECAST(MODEL Covid19NYT.deaths_arima_model,
-    #             STRUCT(30 AS horizon, 0.8 AS confidence_level))
-    # )
-    # """
-    # query_job = bqclient.query(query_string)
-    # results = query_job.result()
     # for row in results:
-    #     DeathsPredictionDate = row.PredDate
-    #     PredDeaths = row.Deaths
+    #     Movie_Review_Text = row.user_review
+    #     Pred_label = row.predicted_review_label
 
-    # #get most recent deaths
-    # query_string= """
-    # SELECT
-    # cast(deaths_date_history as TIMESTAMP) AS deaths_date_history,
-    # total_deaths_history as deaths_history_value,
+    val = {"MovieReviewUserInput":user_review, "PredictedSentimentCategory":results}
 
-    # FROM
-    # (
-    # SELECT
-    # date as deaths_date_history,
-    # SUM(daily_deaths) AS total_deaths_history
-    # FROM
-    # `week5-393522.Covid19NYT.DailyCasesDeaths`
-    # GROUP BY date
-    # ORDER BY date ASC
-    # ) dcd
-    # where dcd.deaths_date_history = (select max(date)
-    # FROM `week5-393522.Covid19NYT.DailyCasesDeaths`)
-    # """
-    # query_job = bqclient.query(query_string)
-    # results = query_job.result()
-    # for row in results:
-    #     DeathsDate = row.deaths_date_history
-    #     QtyDeaths = row.deaths_history_value
-
-    # #get most recent cases
-    # query_string= """
-    # SELECT
-    # cast(cases_date_history as TIMESTAMP) AS cases_date_history,
-    # total_cases_history as cases_history_value,
-
-    # FROM
-    # (
-    # SELECT
-    # date as cases_date_history,
-    # SUM(daily_confirmed_cases) AS total_cases_history
-    # FROM
-    # `covid19-302022.Covid19NYT.DailyCasesDeaths`
-    # GROUP BY date
-    # ORDER BY date ASC
-    # ) dcd
-    # where dcd.cases_date_history = (select max(date)
-    # FROM `covid19-302022.Covid19NYT.DailyCasesDeaths`)
-    # """
-    # query_job = bqclient.query(query_string)
-    # results = query_job.result()
-    # for row in results:
-    #     CasesDate = row.cases_date_history
-    #     QtyCases = row.cases_history_value
-
-    
-    # this would not present the data, want to debug later
-    #val2 = jsonify(CasesPredictionDate=CasesPredictionDate.strftime('%m/%d/%Y'),
-    #              PredictedCases=PredCases,
-    #              DeathsPredictionDate=DeathsPredictionDate.strftime('%m/%d/%Y'),
-    #              PredictedDeaths=PredDeaths,
-    #              DeathsDate=DeathsDate.strftime('%m/%d/%Y'),
-    #              MostRecentDeathCount=QtyDeaths,
-    #              CasesDate=CasesDate.strftime('%m/%d/%Y'),
-    #              MostRecentCasesCount=QtyCases)
-
-    val = {"CasesPredictionDate":CasesPredictionDate.strftime('%m/%d/%Y'), "PredictedCases":PredCases
-        #     "DeathsPredictionDate":DeathsPredictionDate.strftime('%m/%d/%Y'),"PredictedDeaths":PredDeaths,
-        #    "DeathsDate":DeathsDate.strftime('%m/%d/%Y'),"MostRecentDeathCount":QtyDeaths,
-        #    "CasesDate":CasesDate.strftime('%m/%d/%Y'),"MostRecentCasesCount":QtyCases
-        }
-
-
-    return render_template("page.html", title="COVID23: Current Stats and Predictions",jsonfile = val )
+    return render_template("page.html", title="Predicted Movie Review Sentiment Category",jsonfile = val)
 
   
 if __name__ == '__main__':
